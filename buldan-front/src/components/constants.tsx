c1780c4597d3f59f2f8630eb6724a3a260a12cc0
Sami Tabet <salph.tabet@gmail.com>
2024-07-28 15:25:12 +0200

    first commit

---
+++ buldan-front/src/components/constants.tsx
@@ -0,0 +1,4 @@
+export const primaryColor= "bg-blue-500";
+export const secondaryColor= "bg-orange-500";
+export const primaryColorTxt = "text-blue-500";
+export const secondaryColorTxt = "text-orange-500";
